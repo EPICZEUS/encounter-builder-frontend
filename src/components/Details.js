4c19ec002207635abce5286a269a68cdd45b34c9
EPICZEUS <s77broz@gmail.com>
2019-01-22 11:26:07 -0500

    Fix details

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,10 +6,10 @@ const Details = (props) => {
     <Card>
       <Card.Content>
         <Card.Header>{props.creature.name}</Card.Header>
-        <Card.Meta>{props.creature.creature_type}</Card.Meta><hr />
+        <Card.Meta>{props.creature.creature_type || props.creature.player_class}</Card.Meta><hr />
         <Card.Description><span>STR: {props.creature.str}</span>  <span>CON: {props.creature.con}</span>  <span>DEX: {props.creature.dex}</span></Card.Description><br/>
         <Card.Description><span>INT: {props.creature.int}</span>  <span>WIS: {props.creature.wis}</span>  <span>CHA: {props.creature.cha}</span></Card.Description><hr />
-        <Card.Description>Challenge Rating: {props.creature.cr}</Card.Description>
+        <Card.Description>{props.creature.cr != null ? "Challenge rating: " + props.creature.cr : "Level: " + props.creature.level}</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <a>
--- src/containers/PlayerList.js
+++ src/containers/PlayerList.js
@@ -2,10 +2,22 @@
 import React, { Component } from 'react';
 import { Card, Segment } from 'semantic-ui-react';
 import Player from '../components/Player';
+import InfoBox from './InfoBox';
 /* eslint-enable no-unused-vars */
 
 export default class PlayerList extends Component {
+  state = {
+    creatures: [],
+    selected: null
+  }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/creatures')
+    .then(res => res.json())
+    .then(creatures => {
+      this.setState({ creatures })
+    })
+  }
 
 	render() {
 		return (
@@ -20,8 +32,10 @@ export default class PlayerList extends Component {
 						)}
 					</Card.Group>
 				</Segment>
-				<Segment placeholder>
-
+				<Segment>
+          {this.state.selected
+            ? <InfoBox creature={this.state.creatures.find(c => c.id === this.state.selected)} />
+          : <InfoBox creature={null}/>}
 				</Segment>
 			</div>
 		);
